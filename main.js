dc2313608e20c67d99ff215673ffa9b2cd44b928
Yasir Mohamed <mohamey@tcd.ie>
2016-09-22 22:32:57 +0100

    Hooked up form submission to mysql database

--- main.js
+++ main.js
@@ -1,116 +1,136 @@
-var express = require('express');
-var fs = require('fs');
-var url = require('url');
-var images = require('images');
-var cookieParser = require('cookie-parser');
-var randomString = require('randomstring');
-var mysql = require('mysql');
-var http = require('http');
+const express = require('express');
+const fs = require('fs');
+const url = require('url');
+const images = require('images');
+const cookieParser = require('cookie-parser');
+const randomString = require('randomstring');
+const mysql = require('mysql');
+const http = require('http');
+const bodyParser = require('body-parser')
+const validator = require('validator')
+const createDOMPurify = require('dompurify')
+const jsdom = require('jsdom')
+const window = jsdom.jsdom('', {
+  features: {
+    FetchExternalResources: false,
+    ProcessExternalResources: false
+  }
+}).defaultView
+const DOMPurify = createDOMPurify(window)
 
-var app = express();
+const app = express();
 app.use(cookieParser());
 app.use(express.static('public'));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded())
 
 // MySQL Connection
-var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'yasir',
-    password: 'Magbouls2305',
-    database: 'tracking'
+const trackingConnection = mysql.createConnection({
+  host: 'localhost',
+  user: process.argv[2],
+  password: process.argv[3],
+  database: 'tracking'
 });
 
-connection.connect();
+const formConnection = mysql.createConnection({
+  host: 'localhost',
+  user: process.argv[2],
+  password: process.argv[3],
+  database: 'mohamey_forms'
+});
+// Connect to MySql Database
+// try{
+//   connection.connect();
+// }catch(e){
+//   console.err(e)
+// }
 
 app.on('request', function(req, res){
-	console.log("REQUEST: "+req.url);
+  // console.log("REQUEST: "+req.url);
 });
 
 app.get('/', function(req, res){
-    //Handle cookies
-    if(req.cookies.cid == undefined){
-        res.cookie('cid', randomString.generate(30), {'path':'/'});
-        console.log("Cookie generated");
-    }else{
-        console.log("Cookies were received");
-        console.log(req.cookies);
-    }
-    res.set('content-type','text/html');
-    res.sendFile(__dirname+'/index.html');
+  // If no cookie has been sent with the request, generate a new one for tge user
+  if(req.cookies.cid == undefined){
+    res.cookie('cid', randomString.generate(30), {'path':'/'});
+  }
+  res.set('content-type','text/html');
+  res.sendFile(__dirname+'/index.html');
 });
 
 app.get('/thanks', function(req, res){
-    if(req.cookies.cid == undefined){
-        res.cookie('cid', randomString.generate(30), {'path':'/'});
-    }else{
-        console.log(req.cookies);
-    }
-    res.set('content-type', 'text/html');
-    res.sendFile(__dirname+'/thanks.html');
+  // If no cookie has been sent with the request, generate a new one for tge user
+  if(req.cookies.cid == undefined){
+    res.cookie('cid', randomString.generate(30), {'path':'/'});
+  }
+  res.set('content-type', 'text/html');
+  res.sendFile(__dirname+'/thanks.html');
 });
+
+// Handle form submissions
+app.post('/thanks', function(req, res){
+  // Save form details to database
+  const formSql = "INSERT INTO form_submissions (Name, Email, Message) VALUES (?, ?, ?)"
+  const formValues = [DOMPurify.sanitize(req.body.name), DOMPurify.sanitize(req.body.email), DOMPurify.sanitize(req.body.message)]
+  const formQuery = mysql.format(formSql, formValues)
+  try{
+    const formSubmission = formConnection.query(formQuery, (err, result) => {
+      if (err) {
+        console.log(err)
+      }
+    })
+  } catch (err) {
+    console.err(err)
+  }
+
+  res.set('content-type', 'text/html')
+  res.sendFile(`${__dirname}/thanks.html`)
+})
 
 app.get('/snake', function(req, res){
-    res.set('content-type', 'text/html');
-    res.sendFile(__dirname+'/GAMEZ.html');
-});
-
-app.get('/other-page', function(req, res){
-    if(req.cookies.cid == undefined){
-        res.cookie('cid', randomString.generate(30), {'path':'/'});
-    }else{
-        console.log(req.cookies);
-    }
-    res.set('content-type', 'text/html');
-    res.sendFile(__dirname+'/otherPage.html');
-});
-
-app.get('/test', function(req, res){
-	res.set('content-type', 'text/html');
-	res.sendFile(__dirname+'/test.html');
-});
-
-app.get('/embedded', function(req, res){
-	res.set('content-type', 'text/html');
-	res.sendFile(__dirname+'/embedded.html');
+  // If no cookie has been sent with the request, generate a new one for tge user
+  if(req.cookies.cid == undefined){
+    res.cookie('cid', randomString.generate(30), {'path':'/'});
+  }
+  res.set('content-type', 'text/html');
+  res.sendFile(__dirname+'/GAMEZ.html');
 });
 
 app.get('/tracking-pixel', function(request, result){
-    var domain = request.hostname;
-    var ip = request.ip;
-    var useragent = request.headers['user-agent'];
-    
-    //Read url parameters
-    var title = "";
-    var link = request.query.url;
-    title = request.query.title;
+  // If no cookie has been sent with the request, generate a new one for tge user
+  if(req.cookies.cid == undefined){
+    res.cookie('cid', randomString.generate(30), {'path':'/'});
+  }
 
-    images(1,1)
-          .fill(255,255,255)
-          .save('p.jpg');
-    result.set('content-type','image/jpg');
-    result.sendFile(__dirname+'/p.jpg');
-    
-    var usersSql = "INSERT INTO users (cookie, ip_address, user_agent) VALUES (?, ? ,?)";
-    var userValues = [request.cookies.cid, request.ip, request.headers['user-agent']];
+  images(1,1)
+    .fill(255,255,255)
+    .save('p.jpg');
+  result.set('content-type','image/jpg');
+  result.sendFile(__dirname+'/p.jpg');
 
-    var historySql = "INSERT INTO user_history (cookie, domain, history, page_title) VALUES (?, ? ,?, ?)";
-    var historyValues = [request.cookies.cid, domain, link, title];
-    usersSql = mysql.format(usersSql, userValues);
-    historySql = mysql.format(historySql, historyValues);
-    
-    var userQuery = connection.query(usersSql, function(err, result){
-        console.log("Updated users database");
-    });
+  var usersSql = "INSERT INTO users (cookie, ip_address, user_agent) VALUES (?, ? ,?)";
+  var userValues = [request.cookies.cid, request.ip, request.headers['user-agent']];
 
-    var historyQuery = connection.query(historySql, function(err, result){
-        console.log("Updated history database");
-    });
+  var historySql = "INSERT INTO user_history (cookie, domain, history, page_title) VALUES (?, ? ,?, ?)";
+  var historyValues = [request.cookies.cid, request.hostname, request.query.url, request.query.title];
+  usersSql = mysql.format(usersSql, userValues);
+  historySql = mysql.format(historySql, historyValues);
 
-    console.log('Domain: '+domain);
-    console.log('IP Address: '+ip);
-    console.log('User Agent: '+useragent);
-    console.log('Url: '+link);
-    console.log('Title: '+title);
+  var userQuery = trackingConnection.query(usersSql, function(err, result){
+    // console.log("Updated users database");
+    if (err) {
+      console.err(err)
+    }
+  });
+
+  var historyQuery = trackingConnection.query(historySql, function(err, result){
+    // console.log("Updated history database");
+    if (err){
+      console.err(err)
+    }
+  });
 });
 
-http.createServer(app).listen(8080, '10.131.24.117');
-console.log("Server running at http://10.131.24.117:8080/");
+app.listen(8080, () => {})
+// http.createServer(app).listen(8000, 'localhost');
+console.log("Server running at http://127.0.0.1:8080/");
