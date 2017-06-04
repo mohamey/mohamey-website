9c40b05c386c5a4639704490054f24efd1ab1e91
Yasir Mohamed <mohamey@tcd.ie>
2017-06-04 01:21:00 +0100

    Learning Log dynamically changes artical based on selected plot point

--- public/javascript/learningScript.js
+++ public/javascript/learningScript.js
@@ -1,16 +1,19 @@
 $(document).ready(function () {
+    allData = []
     $.getJSON("../resources/data.json", function(storyData) {
-            // Graph scripts here
-            var graphData = [{
-                data: [],
-                color: '#71c73e'
-            }]
-            storyData.forEach(function (entry) {
-                console.log(entry)
-                // var obj = JSON.parse(entry)
-                graphData[0]["data"].push([new Date(entry["date"]), entry["score"]])
-            })
-            // console.log(allData)
+        // Graph scripts here
+        allData = storyData
+        console.log(allData)
+        var graphData = [{
+            data: [],
+            color: '#71c73e'
+        }]
+        storyData.forEach(function (entry) {
+            console.log(entry)
+            // var obj = JSON.parse(entry)
+            graphData[0]["data"].push([new Date(entry["date"]), entry["score"]])
+        })
+        // console.log(allData)
         //   var graphData = [{
         //     // Experience
         //     data: [[new Date("2017/06/01").getTime(), 6], [new Date("2017/06/02").getTime(), 8], [new Date("2017/06/05").getTime(), 4] ],
@@ -33,7 +36,8 @@ $(document).ready(function () {
                 color: '#646464',
                 borderColor: 'transparent',
                 borderWidth: 20,
-                hoverable: true
+                hoverable: true,
+                clickable: true
             },
             xaxis: {
                 tickColor: 'transparent',
@@ -60,7 +64,8 @@ $(document).ready(function () {
                 color: '#646464',
                 borderColor: 'transparent',
                 borderWidth: 20,
-                hoverable: true
+                hoverable: true,
+                clickable: true
             },
             xaxis: {
                 tickColor: 'transparent',
@@ -80,7 +85,7 @@ $(document).ready(function () {
 
         var previousPoint = null;
 
-        $('#graph-lines, #graph-bars').bind('plothover', function (event, pos, item) {
+        $('#graph-lines').bind('plothover', function (event, pos, item) {
             if (item) {
                 if (previousPoint != item.dataIndex) {
                     previousPoint = item.dataIndex;
@@ -94,6 +99,37 @@ $(document).ready(function () {
                 previousPoint = null;
             }
         });
+
+        // $("#graph-lines").bind("plotclick", function (event, pos, item) {
+        //     if (item) {
+        //         console.log(item)
+        //         alert(item.series.label)
+        //     }
+        // })
+        $('#graph-lines').bind('plotclick', function (event, pos, item) {
+            console.log(allData)
+            if (item) {
+                var x = item.datapoint[0],
+                    y = item.datapoint[1];
+                var res;
+                for(var i=0; i < allData.length; i++) {
+                    tmpDate = new Date(allData[i]['date']).getTime()
+                    console.log(tmpDate)
+                    console.log(x)
+                    if (tmpDate == x){
+                        // Change article contents
+                        document.getElementById('story-title').innerHTML = allData[i]['story-title']
+                        document.getElementById('story-date').innerHTML = allData[i]['date']
+                        document.getElementById('story-author').innerHTML = allData[i]['story-author']
+                        document.getElementById('story-image').src = allData[i]['story-img']
+                        document.getElementById('story-content').innerHTML = allData[i]['story-content']
+                        break
+                    }
+                }
+                console.log("Clicked!")
+                console.log(res)
+            }
+        })
 
         $('#graph-bars').hide();
 
